6e1dea54b2a5135031714abdaf2aea82f94cd193
docXaline <aline.raviart@gmail.com>
2025-04-13 23:22:08 +0200

    Delete Projet/Projet directory

--- Projet/Projet/js/pagedebut.js
+++
@@ -1,8 +0,0 @@
-// Fonction pour ouvrir un jeu
-function openGame(gameName) {
-    let nomInput = document.getElementById('nom');
-    let nom = nomInput ? nomInput.value.trim() : ''; // Récupérer la valeur du champ "nom"
-    // Redirection directe vers la page du jeu
-    window.location.href = `${gameName}.php?nom=${nom}`;
-}
-
